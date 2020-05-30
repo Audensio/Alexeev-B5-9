c1ef02d3ae6ea7f233eacc982198258dd361fe31
Audensio <61151385+Audensio@users.noreply.github.com>
2020-05-31 01:04:12 +0300

    Add files via upload

---
+++ B5-9.py
@@ -0,0 +1,17 @@
+import time
+
+def time_this(num_runs=10):
+    def decorator(func_to_run):
+        def func(*args, **kwargs):
+            avg = 0
+            for _ in range(num_runs):
+                t0 = time.time()
+                func_to_run(*args, **kwargs)
+                t1 = time.time()
+                avg += (t1 - t0)
+            avg /= num_runs
+            fn = func_to_run.__name__
+            print("Среднее время выполнения %s за %s запусков: %.5f секунд" % (fn, num_runs, avg))
+        return func
+
+    return decorator
